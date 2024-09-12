38b9c40c1dfc73a8d94bbae16ebe61cc26156a97
decipher3114 <shauryasiwach26@gmail.com>
2024-09-12 17:09:53 +0530

    fix: set minimum version for macOS

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET=10.13");
     #[cfg(target_os = "windows")]
     {
         let mut res = winresource::WindowsResource::new();
